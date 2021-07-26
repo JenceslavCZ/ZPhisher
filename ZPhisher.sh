3d2bb5ff318b2243e396a991d35863ea767f9947
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-07-26 16:36:20 +0530

    Update ZPhisher.sh
    B

--- ZPhisher.sh
+++ ZPhisher.sh
@@ -1,6 +1,8 @@
+clear
 git clone https://github.com/Vretlee/Love
 cd Love
 bash Rawan.sh
+clear
 git clone https://github.com/htr-tech/zphisher
 cd zphisher
 termux-open https://instagram.com/krish_na_2568
